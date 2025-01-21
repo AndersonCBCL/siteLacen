9f80801a33ff1cffb0d47959d1cac64c2752e2c4
lucas <nemokyto@gmail.com>
2025-01-21 13:53:13 -0300

    adicionado: slider automático no banner e slider de imagens nas notícias

--- html/index.php
+++ html/index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LACEN-PE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../estilo/estilo.css">
+    <link href="'https://fonts.googleapis.com/css2?family=Calistoga&family=Inter:wght@100..900&display=swap"
 </head>
 
 <body>
@@ -18,9 +20,19 @@
 
         <main>
             <section class="secao-slides">
-                <!--<img src="../imagens/slides/slides-img1.png" alt="">-->
-                <img src="../imagens/home/imagem1a.png" alt="LACEN-PE">
-
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="../imagens/slides/slides-img1.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="../imagens/home/imagem1a.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="../imagens/slides/slides-img1.png" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                </div>
             </section>
 
             <section class="secao-informacoes">
@@ -55,7 +67,44 @@
                         </div>
                     
                     <div class="ultimas-noticias-img">
-                        <img src="../imagens/img-ultimas-noticias.png" alt="">
+                        <div id="carouselExampleCaptions" class="carousel slide">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                <img src="../imagens/img-ultimas-noticias.png" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Primeira Notícia</h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quos quae ea excepturi eius dolorum saepe ut non magni aspernatur voluptatem hic possimus omnis, facilis vitae magnam recusandae cum laudantium.</p>
+                                </div>
+                                </div>
+                                <div class="carousel-item">
+                                <img src="../imagens/img-ultimas-noticias.png" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Segunda Notícia</h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dicta facere, aliquam non rerum maxime delectus odio assumenda voluptas fugiat repellat, molestiae exercitationem sunt quis?</p>
+                                </div>
+                                </div>
+                                <div class="carousel-item">
+                                <img src="../imagens/img-ultimas-noticias.png" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Terceira Notícia</h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quibusdam delectus saepe accusamus natus libero autem soluta sit cum, labore impedit, mollitia laboriosam dolore quis quasi!.</p>
+                                </div>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                            </div>
                     </div>
                 </div>
             </section>
@@ -96,6 +145,8 @@
     <?php
         include "rodape.html";
     ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
