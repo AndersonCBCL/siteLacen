2fef3b4c4922951ebcf155c2c18e65225d217c0f
lucas <nemokyto@gmail.com>
2025-01-23 11:25:26 -0300

    modificado: barra de pesquisa e botão "Buscar"

--- html/cabecalho.php
+++ html/cabecalho.php
@@ -1,4 +1,9 @@
-<a href="index.php">    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="../estilo/estilo.css">      
+        
+        
+        
+        <a href="index.php">    
             <div class="logo-lacen">
                 <img src="../imagens/logos1/lacen_logo1_transp.png" alt="LACEN-PE">
             </div>
@@ -9,14 +14,16 @@
                 <li><a href="index.php">Início</a></li>
                 <li class=""><a href="institucional.php">Institucional</a></li>
                 <li class=""><a href="areas_lacen.php">Divis&otilde;es</a></li>
-                <li class=""><a href="unidades_lacen.php">Unidades Lacen</a></li>
+                <li class=""><a href="unidades_lacen.php">Unidades  </a></li>
                 <!--<li class=""><a href="missao.php">Missão, Visão e Valores</a></li>-->
                 <li class=""><a href="contato.php">Contato</a></li>
             </ul>
 
             <div class="pesquisa-site">
-                <input type="search" name="pesquisa-site" id="pesquisa-site" placeholder="O que você está procurando?">
-                <a href=""><img src="../imagens/lupas/lupa1.png" alt="Pesquisa no site"></a>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Procurando algo?" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
             </div>
 
         </nav>
@@ -27,3 +34,5 @@
             </div>
         </div>
 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
