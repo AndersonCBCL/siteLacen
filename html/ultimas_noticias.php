21bbb0bd2da5bac648d4d2270bba25def29cc219
lucas <nemokyto@gmail.com>
2025-01-31 14:49:52 -0300

    modificado: página de notícias reformulada

--- html/ultimas_noticias.php
+++ html/ultimas_noticias.php
@@ -4,170 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Últimas Notícias</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="../estilo/estilo.css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
-
     <header>
         <?php include "cabecalho.php" ?>
     </header>
     
     <div class="container">
         
-    <section class="banner-sec">
-        <div class="container">
-    <div class="row">
-            <div class="col-md-3">
-        <div class="card"> <img class="img-fluid" src="" alt="">
-                <div class="card-body">
-            <div class="news-title">
-                    <h2 class=" title-small" id="openPopup-1">Notícia Antiga 1</h2>
-                  </div>
-            <p class="card-text"><small class="text-time"><em>3h atrás</em></small></p>
-          </div>
-              </div>
-        <div class="card"> <img class="img-fluid" src="" alt="">
-                <div class="card-body">
-            <div class="news-title">
-                    <h2 class=" title-small" id="openPopup-2">Notícia Antiga 2</h2>
-                  </div>
-            <p class="card-text"><small class="text-time"><em>3h atrás</em></small></p>
-          </div>
-              </div>
-      </div>
-            <div class="col-md-3">
-        <div class="card"> <img class="img-fluid" src="" alt="">
-                <div class="card-body">
-            <div class="news-title">
-                    <h2 class=" title-small" id="openPopup-3">Notícia Antiga 3</h2>
-                  </div>
-            <p class="card-text"><small class="text-time"><em>3h atrás</em></small></p>
-          </div>
-              </div>
-        <div class="card"> <img class="img-fluid" src="" alt="">
-                <div class="card-body">
-            <div class="news-title">
-                    <h2 class=" title-small" id="openPopup-4">Notícia Antiga 4</h2>
-                  </div>
-            <p class="card-text"><small class="text-time"><em>3h atrás</em></small></p>
-          </div>
-              </div>
-      </div>
-         <!-- não tirar os botões daqui nem tentar organizar o código, se não para de funcionar -->
-            <div class="col-md-6 top-slider">
-            <div id="carouselExampleIndicators" class="carousel slide">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <nav class="nav_paginas">
+            <ul>
+                <li>
+                    <a href="index.php"> Home</a>
+                </li>
+                <span class="dir">></span> 
+                <li>
+                    <a href="Index.php"> &Uacute;ltimas Not&iacute;cias </a>
+                </li>
+            </ul>
+        </nav>
+    
+        <div class="pagina_ultimas_noticias">
+            <h1>&Uacute;ltimas Not&iacute;cias</h1>
+            <section class="ultimas_noticias">
+                <div class="destaque">
                 </div>
-          </ol>
-                
-                <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-                    <div class="news-block">
-                <div class="news-media"><img class="img-fluid" src="../imagens/ultimas-noticias/WhatsApp-Image-2020-08-28-at-15.07.13.jpeg" alt="..." style="width: 455px; height: 275px;"></div>
-                <div class="news-title">
-                        <h2 class=" title-large" id="openPopup-5">Notícia Nova 1</h2>
-                      </div>
-                <div class="news-des">Condimentum ultrices mi est a arcu at cum a elementum per cum turpis dui vulputate vestibulum in vehicula montes vel. Mauris nam suspendisse consectetur mus...</div>
-                <div class="time-text"><strong>Agora</strong></div>
-                <div></div>
-              </div>
-                  </div>
-            <div class="carousel-item">
-                    <div class="news-block">
-                <div class="news-media"><img class="img-fluid" src="../imagens/ultimas-noticias/LACEN (4)-1.jpg" alt="..." style="width: 455px; height: 275px;"></div>
-                <div class="news-title">
-                        <h2 class=" title-large" id="openPopup-6">Notícia Nova 2</h2>
-                      </div>
-                <div class="news-des">Condimentum ultrices mi est a arcu at cum a elementum per cum turpis dui vulputate vestibulum in vehicula montes vel. Mauris nam suspendisse consectetur mus...</div>
-                <div class="time-text"><strong>2h atrás</strong></div>
-                <div></div>
-              </div>
-                  </div>
-            <div class="carousel-item">
-                    <div class="news-block">
-                <div class="news-media"><img class="img-fluid" src="../imagens/img-ultimas-noticias.png" alt="..." style="width: 455px; height: 275px;" alt=""></div>
-                <div class="news-title">
-                        <h2 class=" title-large" id="openPopup-7">Notícia Nova 3</h2>
-                      </div>
-                <div class="news-des">Condimentum ultrices mi est a arcu at cum a elementum per cum turpis dui vulputate vestibulum in vehicula montes vel. Mauris nam suspendisse consectetur mus...</div>
-                <div class="time-text"><strong>2h </strong></div>
-                <div></div>
-              </div>
-                  </div>
-          </div>
-              </div>
-      </div>
-          </div>
-  </div>
-      </section>
-        
-    </div>
+                <div class="noticias_anteriores"></div>
+            </section>
+        </div>
 
-    <div id="popup-1" class="popup">
-            <div class="popup-conteudo">
-            <span class="fechar" id="closePopup-1">&times;</span>
-            <h2>Notícia Antiga 1</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae itaque, cum veniam provident minus.</p>
-            </div>
-        </div>
-    <div id="popup-2" class="popup">
-            <div class="popup-conteudo">
-            <span class="fechar" id="closePopup-2">&times;</span>
-            <h2>Notícia Antiga 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae itaque, cum veniam provident minus.</p>
-            </div>
-        </div>
-    <div id="popup-3" class="popup">
-            <div class="popup-conteudo">
-            <span class="fechar" id="closePopup-3">&times;</span>
-            <h2>Notícia Antiga 3</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae itaque, cum veniam provident minus.</p>
-            </div>
-        </div>
-    <div id="popup-4" class="popup">
-            <div class="popup-conteudo">
-            <span class="fechar" id="closePopup-4">&times;</span>
-            <h2>Notícia Antiga 4</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae itaque, cum veniam provident minus.</p>
-            </div>
-        </div>
-    <div id="popup-5" class="popup">
-            <div class="popup-conteudo">
-            <span class="fechar" id="closePopup-5">&times;</span>
-            <h2>Notícia Nova 1</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            <img src="../imagens/ultimas-noticias/WhatsApp-Image-2020-08-28-at-15.07.13.jpeg" alt="..." style="width: 600px; height: 400px;">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio corporis quod rem laborum commodi dolorum cumque tempore, eum exercitationem eaque provident aspernatur nulla incidunt perferendis reiciendis perspiciatis aperiam sapiente!</p>
-            </div>
-        </div>
-    <div id="popup-6" class="popup">
-            <div class="popup-conteudo">
-            <span class="fechar" id="closePopup-6">&times;</span>
-            <h2>Notícia Nova 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <img src="../imagens/ultimas-noticias/LACEN (4)-1.jpg" alt="..." style="width: 600px; height: 400px;" >
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae itaque, cum veniam provident minus.</p>
-            </div>
-        </div>
-    <div id="popup-7" class="popup">
-            <div class="popup-conteudo">
-            <span class="fechar" id="closePopup-7">&times;</span>
-            <h2>Notícia Nova 3</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <img src="../imagens/img-ultimas-noticias.png" alt="..." style="width: 600px; height: 400px;">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae itaque, cum veniam provident minus.</p>
-            </div>
-        </div>
+        <div class="news-container" id="newsContainer"></div>
+        <div class="pagination" id="pagination"></div>
+        <script src="../src/noticias.js"></script>
+    </div>
     <?php
         include "rodape.html";
     ?>
-
-    <script src="../src/noticias.js"></script>
 </body>
 </html>
--- src/noticias.js
+++ src/noticias.js
@@ -1,27 +1,128 @@
-function setupPopup(popupId, openButtonId, closeButtonId) {
-    const popup = document.getElementById(popupId);
-    const openButton = document.getElementById(openButtonId);
-    const closeButton = document.getElementById(closeButtonId);
+const noticias = [
+  { 
+    titulo: "Teste 1", 
+    descricao: "Descrição 1", 
+    data: "30/01/2025", 
+    link: "noticia1.html",
+    imagem: "../imagens/ultimas-noticias/WhatsApp-Image-2020-08-28-at-15.07.13.jpeg"
+  },
+  { 
+    titulo: "Teste 2", 
+    descricao: "Descrição 2", 
+    data: "29/01/2025", 
+    link: "noticia2.html", 
+    imagem: "../imagens/ultimas-noticias/LACEN (4)-1.jpg"
+  },
+  { 
+    titulo: "Teste 3", 
+    descricao: "Descrição 3", 
+    data: "30/01/2025", 
+    link: "noticia1.html",
+    imagem: "../imagens/img-ultimas-noticias.png"
+  },
+  { 
+    titulo: "Teste 4", 
+    descricao: "Descrição 4", 
+    data: "30/01/2025", 
+    link: "noticia1.html",
+  },
+  { 
+    titulo: "Teste 5", 
+    descricao: "Descrição 5", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+  { 
+    titulo: "Teste 6", 
+    descricao: "Descrição 6", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+  { 
+    titulo: "Teste 7", 
+    descricao: "Descrição 7", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+  { 
+    titulo: "Teste 8", 
+    descricao: "Descrição 8", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+  { 
+    titulo: "Teste 9", 
+    descricao: "Descrição 9", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+  { 
+    titulo: "Teste 10", 
+    descricao: "Descrição 10", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+  { 
+    titulo: "Teste 11", 
+    descricao: "Descrição 11", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+  { 
+    titulo: "Teste 12", 
+    descricao: "Descrição 12", 
+    data: "29/01/2025", 
+    link: "noticia2.html" 
+  },
+
+];
+
+const noticiasPorPagina = 10;
+let paginaAtual = 1;
+
+function exibirNoticias() {
+  const container = document.getElementById("newsContainer");
+  container.innerHTML = "";
   
-    openButton.addEventListener('click', () => {
-      popup.style.display = 'flex';
-    });
+  const inicio = (paginaAtual - 1) * noticiasPorPagina;
+  const fim = inicio + noticiasPorPagina;
+  const noticiasExibir = noticias.slice(inicio, fim);
+
+  noticiasExibir.forEach(noticia => {
+      const div = document.createElement("div");
+      div.className = "news-item";
+
+      const imagemHTML = noticia.imagem ? `<img src="${noticia.imagem}" alt="Imagem da notícia">` : "";
+
+      div.innerHTML = `
+          <h2><a href="${noticia.link}" target="_blank">${noticia.titulo}</a></h2>
+          <p>${noticia.descricao}</p>
+          <small>${noticia.data}</small>
+          ${imagemHTML}
+      `;
+      container.appendChild(div);
+  });
+
+  atualizarPaginacao();
+}
+
+function atualizarPaginacao() {
+  const pagContainer = document.getElementById("pagination");
+  pagContainer.innerHTML = "";
+  const totalPaginas = Math.ceil(noticias.length / noticiasPorPagina);
   
-    closeButton.addEventListener('click', () => {
-      popup.style.display = 'none';
-    });
-  
-    popup.addEventListener('click', (event) => {
-      if (event.target === popup) {
-        popup.style.display = 'none';
-      }
-    });
+  for (let i = 1; i <= totalPaginas; i++) {
+      const link = document.createElement("a");
+      link.href = "#";
+      link.innerText = i;
+      link.className = i === paginaAtual ? "active" : "";
+      link.onclick = function (event) {
+          event.preventDefault(); // Evita o recarregamento da página ao clicar
+          paginaAtual = i;
+          exibirNoticias();
+      };
+      pagContainer.appendChild(link);
   }
-  
-  setupPopup('popup-1', 'openPopup-1', 'closePopup-1');
-  setupPopup('popup-2', 'openPopup-2', 'closePopup-2');
-  setupPopup('popup-3', 'openPopup-3', 'closePopup-3');
-  setupPopup('popup-4', 'openPopup-4', 'closePopup-4');
-  setupPopup('popup-5', 'openPopup-5', 'closePopup-5');
-  setupPopup('popup-6', 'openPopup-6', 'closePopup-6');
-  setupPopup('popup-7', 'openPopup-7', 'closePopup-7');
+}
+
+exibirNoticias();
