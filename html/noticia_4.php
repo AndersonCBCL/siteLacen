da5de9e7556b0d01607f929353a34ea995c79308
lucas <nemokyto@gmail.com>
2025-02-12 11:57:04 -0300

    adicionado: páginas-fonte das notícias

---
+++ html/noticia_4.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LACEN-PE</title>
+    <link rel="stylesheet" href="../estilo/estilo.css">
+</head>
+<body>
+    
+    <article id="primeira" class="column-middle">
+                <h1 class="ultima_noticia">Texto de Exemplo</h1>
+                <p class="primeiro_paragrafo">
+                    Lorem ipsum odor amet, consectetuer adipiscing elit. Aptent etiam curae justo accumsan nascetur phasellus pellentesque. Id lorem rhoncus sem ornare cras. Enim accumsan senectus lectus pretium ligula sociosqu tristique faucibus. Ridiculus nullam maecenas, elementum congue nulla penatibus. Bibendum hendrerit class vehicula ipsum urna.
+                </p>
+                 <span>06/02/2025</span>
+                <p>
+                    Neque nisi ex imperdiet integer maximus vivamus varius imperdiet. Torquent laoreet fames erat nulla primis. Sed dis sed est ut morbi feugiat nisi. Condimentum id pharetra ipsum ornare tortor a. Urna magna nulla augue massa ipsum venenatis. Nec odio interdum montes venenatis rutrum facilisis phasellus sagittis. Imperdiet fringilla porttitor nisl litora; libero orci aliquet maximus. Dolor nam nulla eget mattis quam sapien eleifend elit metus. Aliquam ipsum fames himenaeos blandit sit faucibus litora gravida.
+                </p>
+                <p>
+                    Ridiculus condimentum magnis imperdiet euismod vestibulum purus semper. Ex aliquam quis gravida dapibus interdum potenti placerat. Nisi fringilla mi; efficitur aenean posuere venenatis. Consectetur elit justo mus velit purus volutpat magnis. Odio est quis nascetur ante porta. Odio sagittis commodo a duis risus. Consectetur augue elit maximus feugiat purus adipiscing nibh. Et felis mattis parturient integer magnis sed; venenatis nibh eros.
+                </p>
+        </article>
+
+
+</body>
+</html>
